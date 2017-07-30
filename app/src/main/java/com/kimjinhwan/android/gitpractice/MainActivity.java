83cf62ff1583ccd55bc476a746fa7edb4b6d89e6
pleasantlife <pleasantlife@gmail.com>
2017-07-30 14:47:04 +0900

    Make Button

--- app/src/main/java/com/kimjinhwan/android/gitpractice/MainActivity.java
+++ app/src/main/java/com/kimjinhwan/android/gitpractice/MainActivity.java
@@ -2,6 +2,7 @@ package com.kimjinhwan.android.gitpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button a = new Button(this);
+        Button b = new Button(this);
+        Button c = new Button(this);
+        Button d = new Button(this);
+        Button e = new Button(this);
+        Button f = new Button(this);
     }
 }
